5b5bc7362f02cd18dd0dcd960772b9e42daa43e6
Przybylski 중광 <r351574nc3@gmail.com>
2013-10-05 12:21:31 -0700

    Converting to a multi-project build

--- project/Build.scala
+++ project/Build.scala
@@ -3,4 +3,16 @@ import sbt._
 
 object RiceWithRedis extends Build {
 
+  lazy val root = 
+    project.in(file(".")).aggregate(app, web, it)
+
+  lazy val app = 
+    project.in(file("app")) 
+
+  lazy val it = 
+    project.in(file("it")).dependsOn(app)
+
+  lazy val web = 
+    project.in(file("web")).dependsOn(app)
+
 }
